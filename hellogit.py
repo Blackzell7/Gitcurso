2651fe6e31ae650826c4dffc01c055564bb9221b
Haz <blackzel117@gmail.com>
2024-08-26 23:06:34 -0600

    cambio de rama para ver como funciona

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,3 @@
 print("hola putos ejemplo de otra cosa, nueva version como guardar un fichero modificado")
 print("ahora vamos a ver como funciona el git diff")
+print("este es para ver como funciona el cambio de rama con un commit")
